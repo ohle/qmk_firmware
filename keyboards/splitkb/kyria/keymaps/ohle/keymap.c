cb13c0baf5ede7954f9ae549f2a7e6e48b1b85fd
Ohle Claussen <claussen@jpk.com>
2022-09-23 17:26:13 +0200

    Fix typos; add backspace and delete

--- keyboards/splitkb/kyria/keymaps/ohle/keymap.c
+++ keyboards/splitkb/kyria/keymaps/ohle/keymap.c
@@ -31,15 +31,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |      |  |      |      |   N  |   M  | , >  | . >  | / ?  | RShift |
  * `----------------------+------+------+------| Sym  | Num  |  | Fun  |      |------+------+------+----------------------'
- *                        | Mouse| Nav  | Enter| Space| Tab  |  | Tab  | Space| Enter|      |      |
+ *                        | Mouse| Nav  | Enter| Space| Tab  |  | Tab  | Space| Enter| bcksp|  del |
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
-    [_QWERTY] = LAYOUT(
+    [_BASE] = LAYOUT(
      PAUSE   , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , XXXXXXX,
-     KC_ESC  , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN,CTL_QUOT,
+     KC_ESC  , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN, KC_QUOT,
      KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , XXXXXXX,XXXXXXX,     XXXXXXX, XXXXXXX, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
-                                MOUSE  ,   NAV  , KC_ENT , SYM_SPC,NUM_TAB,     FUN_TAB, KC_SPC,KC_ENT, XXXXXXX,XXXXXXX
+                                MOUSE  ,   NAV  , KC_ENT , SYM_SPC,NUM_TAB,     FUN_TAB, KC_SPC,KC_ENT, KC_BSPC,KC_DEL
     ),
 /*
  * ,-------------------------------------------.                              ,-------------------------------------------.
@@ -91,7 +91,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [_FUN] = LAYOUT(
       _______, KC_F12 , KC_F7  , KC_F8  , KC_F9  , _______,                                     _______, _______, _______, _______, _______, _______,
-      _______, KC_F11 , KC_F4  , KC_F5  , KC_F6  , _______,                                     _______, KC_RGUI, KC_RALT, KC_CTRL, KC_HYPR, _______,
+      _______, KC_F11 , KC_F4  , KC_F5  , KC_F6  , _______,                                     _______, KC_RGUI, KC_RALT, KC_RCTL, KC_HYPR, _______,
       KC_LSFT, KC_F10 , KC_F1  , KC_F2  , KC_F3  , _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______, _______, _______, KC_RSFT,
                                  _______, _______, _______, KC_SPC , _______, _______, KC_SPC , _______, _______, _______
     ),
@@ -177,7 +177,7 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
         // Accelerated scrolling using up/down arrows
         if (timer_elapsed(scrollTimer) < ACCSCROLL_REPEAT_INTERVAL) {
             scrollStepSize += ACCSCROLL_STEP_SIZE;
-            scrollStepSize  (scrollStepSize > ACCSCROLL_MAX_STEP) ? ACCSCROLL_MAX_STEP : scrollStepSize;
+            scrollStepSize = (scrollStepSize > ACCSCROLL_MAX_STEP) ? ACCSCROLL_MAX_STEP : scrollStepSize;
         } else {
             scrollStepSize = 1;
         }
