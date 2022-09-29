eb9bbd871ca7f1a064f94103276a063d409df82e
Ohle Claussen <claussen@jpk.com>
2022-09-29 17:59:12 +0200

    Enable dynamic macros (and squeeze out some space for it)

--- keyboards/splitkb/kyria/keymaps/ohle/keymap.c
+++ keyboards/splitkb/kyria/keymaps/ohle/keymap.c
@@ -133,7 +133,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
  * |        | Hyper|  Ctrl|  Alt |  Gui | acc1 |                              |  ←   |   ↓  |   ↑  |   →  |      |        |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * | LShift |      |      |      |      | acc0 |      |      |  |      |      |      |      |      |      |      |        |
+ * | LShift |      |      |      |      | acc0 |      |      |  |      |      |  MR1 |  MP1 |  MR2 |  MP2 |      |        |
  * `----------------------+------+------+------|      |      |  |      |      |------+------+------+----------------------'
  *                        |      |      |      | Space|      |  |      | Space|      |      |      |
  *                        | --   |      |      |      |      |  |      |      |      |      |      |
@@ -142,7 +142,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_MOUSE] = LAYOUT(
       _______, _______, _______, _______, _______, KC_ACL2,                                     KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______, _______,
       _______, KC_HYPR, KC_LGUI, KC_LALT, KC_LCTL, KC_ACL1,                                     KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______,
-      LSHFT  , _______, _______, _______, _______, KC_ACL0, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______, _______, _______, _______,
+      LSHFT  , _______, _______, _______, _______, KC_ACL0, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DM_REC1, DM_PLY1, DM_REC2, DM_PLY2, _______, _______,
                                  _______, _______, _______, KC_SPC , _______, KC_BTN1, KC_BTN2, KC_BTN3, _______, _______
     ),
 //  * Layer template
--- keyboards/splitkb/kyria/keymaps/ohle/rules.mk
+++ keyboards/splitkb/kyria/keymaps/ohle/rules.mk
@@ -1,3 +1,11 @@
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 MOUSEKEY_ENABLE = yes
 CAPS_WORD_ENABLE = yes
+DYNAMIC_MACRO_ENABLE = yes
+
+LTO_ENABLE = yes # Link-time optimization to reduce size
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no
+MAGIC_ENABLE = no
