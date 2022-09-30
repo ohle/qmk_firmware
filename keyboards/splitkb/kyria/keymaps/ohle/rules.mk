869db1ced7a9a1dd1c2cbead8567fe55bd1edcf8
Ohle Claussen <claussen@jpk.com>
2022-09-30 16:17:05 +0200

    Restore some options in an attempt to restore encoder functionality

--- keyboards/splitkb/kyria/keymaps/ohle/rules.mk
+++ keyboards/splitkb/kyria/keymaps/ohle/rules.mk
@@ -5,7 +5,7 @@ DYNAMIC_MACRO_ENABLE = yes
 
 LTO_ENABLE = yes # Link-time optimization to reduce size
 CONSOLE_ENABLE = no
-COMMAND_ENABLE = no
+# COMMAND_ENABLE = no
 SPACE_CADET_ENABLE = no
 GRAVE_ESC_ENABLE = no
-MAGIC_ENABLE = no
+# MAGIC_ENABLE = no
