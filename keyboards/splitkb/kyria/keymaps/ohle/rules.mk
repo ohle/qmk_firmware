5694028a757853faa9f4d855eb830420cc978efa
Ohle Claussen <claussen@jpk.com>
2022-10-05 18:39:21 +0200

    There is no compose sequence for π — use unicode

--- keyboards/splitkb/kyria/keymaps/ohle/rules.mk
+++ keyboards/splitkb/kyria/keymaps/ohle/rules.mk
@@ -2,6 +2,7 @@ ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 MOUSEKEY_ENABLE = yes
 CAPS_WORD_ENABLE = yes
 DYNAMIC_MACRO_ENABLE = yes
+UNICODE_ENABLE = yes
 
 LTO_ENABLE = yes # Link-time optimization to reduce size
 CONSOLE_ENABLE = no
