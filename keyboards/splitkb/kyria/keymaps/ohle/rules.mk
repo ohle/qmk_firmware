f5fd273c956568d7d4e0b12be80134053f0c4972
Ohle Claussen <claussen@jpk.com>
2022-10-05 18:24:58 +0200

    Free up a lot of space from features enabled at keyboard level

--- keyboards/splitkb/kyria/keymaps/ohle/rules.mk
+++ keyboards/splitkb/kyria/keymaps/ohle/rules.mk
@@ -5,7 +5,10 @@ DYNAMIC_MACRO_ENABLE = yes
 
 LTO_ENABLE = yes # Link-time optimization to reduce size
 CONSOLE_ENABLE = no
-# COMMAND_ENABLE = no
+COMMAND_ENABLE = no
 SPACE_CADET_ENABLE = no
 GRAVE_ESC_ENABLE = no
 MAGIC_ENABLE = no
+
+OLED_ENABLE = no
+RGBLIGHT_ENABLE = no
