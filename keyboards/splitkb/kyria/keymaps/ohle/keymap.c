a1e7a494609f43a5e7333397bfabb0fc8cbd7f88
Ohle Claussen <claussen@jpk.com>
2023-08-24 08:31:00 +0200

    Use compose key for π
    Requires something like
    
    <Multi_key> <p> <i> "π"
    
    in ~/.XCompose

--- keyboards/splitkb/kyria/keymaps/ohle/keymap.c
+++ keyboards/splitkb/kyria/keymaps/ohle/keymap.c
@@ -22,6 +22,7 @@ enum custom_keycodes {
     C_DEG,
     C_EUR,
     C_MDASH,
+    C_PI,
     C_R,
     C_C,
     C_LEQ,
@@ -271,7 +272,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        `----------------------------------'  `----------------------------------'
  */
     [_ALTSYM] = LAYOUT(
-      _______, C_SQ   , C_CUB  , C_EUR  , C_R    , C_TM   ,                                     C_PM   , C_UE   , _______,    C_OE   , UC(0x03c0), _______,
+
+      _______, C_SQ   , C_CUB  , C_EUR  , C_R    , C_TM   ,                                     C_PM   , C_UE   , _______,    C_OE   , C_PI, _______,
       _______, C_AE   , C_SZ   , C_DEG  , _______, _______,                                     C_NEQ  , _______, _______, _______, _______, _______,
       _______, _______, _______, C_C    , C_SQRT , _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, C_MU   , C_LEQ  , C_GEQ  , _______, _______,
                                  _______, _______, _______, _______, _______, _______, _______, C_MDASH, _______, _______
@@ -458,6 +460,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             break;
         case C_CUB:
             SEND_COMPSEQ("^3");
+            break;
+        case C_PI:
+            SEND_COMPSEQ("pi");
             break;
         case SL_SPC:
             layer_clear();
